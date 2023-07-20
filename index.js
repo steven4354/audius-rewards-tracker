28428bd8978cf624d144c73ed90630097e4b3ded
steven4354 <steven4354@gmail.com>
2023-07-20 18:06:13 -0500

    change the cron tiime

--- index.js
+++ index.js
@@ -8,15 +8,18 @@ dotenv.config();
 
 // Connect to the Ethereum network using Alchemy
 const web3 = new Web3(process.env.ALCHEMY_URL);
+console.log('Connected to Ethereum network');
 
 // Import the contract ABI from a JSON file
 const contractABI = JSON.parse(fs.readFileSync("./contract-abi.json", "utf8"));
+console.log('Contract ABI imported');
 
 // The contract address of audius staking contract
 const contractAddress = "0xe6d97b2099f142513be7a2a068be040656ae4591";
 
 // Create a new contract instance
 const contract = new web3.eth.Contract(contractABI, contractAddress);
+console.log('Contract instance created');
 
 // The account address you want to check
 const accountAddress = "0x61A1BC089f87F1C0e38A34207D65077484d89088";
@@ -31,37 +34,46 @@ let transporter = nodemailer.createTransport({
     pass: process.env.PASSWORD,
   },
 });
+console.log('Transporter object created');
 
 // Schedule a task to run every day at 5pm PT
 // 0 17 * * *
 
 // Schedule a task to run every minute
-// 0 * * * *
+// * * * * *
+
 cron.schedule(
-  "0 * * * *",
+  "0 17 * * *",
   async () => {
-    const lastClaimedFor = await contract.methods
-      .lastClaimedFor(accountAddress)
-      .call();
-    console.log(`Last claimed for: ${lastClaimedFor}`);
+    try {
+      console.log('Task started');
+      const lastClaimedFor = await contract.methods
+        .lastClaimedFor(accountAddress)
+        .call();
+      console.log(`Last claimed for: ${lastClaimedFor}`);
 
-    // Send an email with the last claimed for information
-    let mailOptions = {
-      from: process.env.EMAIL,
-      to: process.env.RECIPIENT_EMAIL,
-      subject: "Last Claimed For Information",
-      text: `Last claimed for: ${lastClaimedFor}`,
-    };
+      // Send an email with the last claimed for information
+      let mailOptions = {
+        from: process.env.EMAIL,
+        to: process.env.RECIPIENT_EMAIL,
+        subject: "Last Claimed For Information",
+        text: `Last claimed for: ${lastClaimedFor}`,
+      };
 
-    transporter.sendMail(mailOptions, function (error, info) {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("Email sent: " + info.response);
-      }
-    });
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.error('Error sending email:', error);
+        } else {
+          console.log("Email sent: " + info.response);
+        }
+      });
+      console.log('Task completed');
+    } catch (error) {
+      console.error('Error in task:', error);
+    }
   },
   {
     timezone: "America/Los_Angeles",
   }
 );
+console.log('Task scheduled');
